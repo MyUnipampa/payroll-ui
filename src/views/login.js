5f8cfed6e1c4b15cae3824d986b013124cf1ccca
Samuel Modesto <samuelmodestoes@gmail.com>
2022-06-13 17:47:45 -0300

    inciando a tela de login

---
+++ src/views/login.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Login extends React.Component{
+    render(){
+        return(
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6" style={{position : 'relative', left: '300px'}}>
+                    Hello
+                </div>
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Login
